73fd7223284af9cce53ad903a7c1fd7fa12e327c
Kaviviane <v.kabanyana@alustudent.com>
2021-08-28 11:18:40 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,8 @@
-#!/bin/bash
 #include <stdio.h>
-puts("Programming is like building a multilingual puzzle);
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return(0);
+}
